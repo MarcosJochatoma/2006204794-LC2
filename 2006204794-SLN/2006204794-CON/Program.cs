27e4fb79e0b2a99b7235816590cf1cc538d1b373
MarcosJochatoma <you@example.com>
2017-05-17 13:50:42 -0500

    Prueba

---
+++ 2006204794-SLN/2006204794-CON/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2006204794_CON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mensaje Prueba");
+        }
+    }
+}
