ccc22ca287f6a283487d49264f9fe2ab73e5d0b4
MarcosJochatoma <you@example.com>
2017-06-10 23:45:38 -0500

    B

--- 2006204794-SLN/2006204794-PER/EntityConfiguration/DispensadorEfectivoConfiguration.cs
+++ 2006204794-SLN/2006204794-PER/EntityConfiguration/DispensadorEfectivoConfiguration.cs
@@ -10,7 +10,14 @@ namespace _2006204794_PER.EntityConfiguration
 {
     public class DispensadorEfectivoConfiguration : EntityTypeConfiguration<DispensadorEfectivo>
     {
+        public DispensadorEfectivoConfiguration()
+        {
 
-       
+            ToTable("DispensadorEfectivo");
+
+            HasKey(a => a.idDispensadorefectivo);
+            HasOptional(c => c.Retiro).WithRequired(c => c.Dispensadorefectivo);
+        }
+
     }
 }
